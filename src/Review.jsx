69fa053a414b84cbc98700770b09a335ae6f5361
Drew Pomatti <ampomatti@gmail.com>
2023-03-28 15:48:24 -0700

    added results, continued to build out functionality, and added styling to login and signup components

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,13 +1,16 @@
 const Review = (props) => {
+    const { createdAt, establishment, address, description, body, rating, reviewer, comments } = props
+
     return (
-        <div>
-            <h1>{props.establishment}</h1>
-            <h2>{props.address}</h2>
-            <h2>{props.description}</h2>
-            <h2>{props.body}</h2>
-            <h2>{props.rating}</h2>
-            <h2>{props.reviewer}</h2>
-            <h2>{props.comments}</h2>
+        <div className="chicken-review">
+            <h1>{establishment}</h1>
+            <h2>{address}</h2>
+            <h2>{description}</h2>
+            <h3>{body}</h3>
+            <h3>{rating}</h3>
+            <h4>{reviewer}</h4>
+            <h4>{createdAt}</h4>
+            <h5>{comments}</h5>
         </div>
     )
 }
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,21 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Results from "./Results";
+const RATINGS = ["🐔", "🐔🐔", "🐔🐔🐔", "🐔🐔🐔🐔", "🐔🐔🐔🐔🐔"]
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Los Angeles, CA")
+    const [location, setLocation] = useState("")
+    const [restaurant, setRestaurant] = useState("")
+    const [rating, setRating] = useState("")
+    const [reviews, setReviews] = useState("")
+
+    useEffect(() => {
+        requestReviews();
+    }, []);
+
+    async function requestReviews() {
+        const res = await fetch(
+            `https://drewery-hot-chicken.onrender.com/api/review/public`
+        );
+        const json = await res.json();
+
+        setReviews(json.reviews);
+    }
+
     return (
         <div className="search-params">
             <form>
+                <h2>Search For Reviews!</h2>
                 <label htmlFor="location">
                     Location
                     <input
                         onChange={(e) => setLocation(e.target.value)}
                         id="location"
                         value={location}
-                        placeholder={location}
+                        placeholder="Location"
                     />
+                </label>
+                <label htmlFor="restaurant">
+                    Restaurant
+                    <input
+                        onChange={(e) => setRestaurant(e.target.value)}
+                        id="restaurant"
+                        value={restaurant}
+                        placeholder="Restaurant"
+                    />
+                </label>
+                <label htmlFor="location">
+                    Rating
+                    <select
+                        id="rating"
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                    >
+                        <option />
+                            {RATINGS.map((rating) => (
+                                <option key={rating}>{rating}</option>
+                            ))}
+                    </select>    
                 </label>
                 <button>Submit</button>
             </form>
+            <Results reviews={reviews} />
         </div>
     )
 }
