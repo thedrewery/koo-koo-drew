dfbeafd10d2ac381a4941000a2a11830f5cd9950
Drew Pomatti <ampomatti@gmail.com>
2023-04-06 19:28:29 -0700

    starting to connect to API

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import cookieParser from "cookie-parser";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [jwt, setJwt] = useState("")
     
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch("https://drewery-hot-chicken.onrender.com/signIn", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        });
+        const data = await response.json();
+        setJwt(data.token);
+    };
+
+//   useEffect(() => {
+//         const csrfToken = cookieParser.parse(document.cookie).csrfToken;
+//         if (jwt) {
+//             fetch("/api/token", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                     "X-CSRF-Token": csrfToken,
+//                 },
+//                 body: JSON.stringify({ token: jwt }),
+//             });
+//         }
+//     }, [jwt]);
+
     return (
         <div className="user-login">
             <h2>Already Have an Account? Sign In!</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">
                     Username
                     <input
